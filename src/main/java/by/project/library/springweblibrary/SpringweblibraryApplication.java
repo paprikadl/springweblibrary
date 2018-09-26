f587cbd61784d608a3008ec819a027880f04f0a1
Andrew Vlasenko <andrewvlasx@gmail.com>
2018-09-27 00:06:51 +0300

    Сreated project and added dependencies

---
+++ src/main/java/by/project/library/springweblibrary/SpringweblibraryApplication.java
@@ -0,0 +1,12 @@
+package by.project.library.springweblibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringweblibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringweblibraryApplication.class, args);
+    }
+}
