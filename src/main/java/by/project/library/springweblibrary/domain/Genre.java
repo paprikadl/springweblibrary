4b2f956f265b8c7442412cd63c3a00295d27d451
Andrew Vlasenko <andrewvlasx@gmail.com>
2018-09-27 00:28:56 +0300

    Added entity classes

---
+++ src/main/java/by/project/library/springweblibrary/domain/Genre.java
@@ -0,0 +1,38 @@
+package by.project.library.springweblibrary.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.SelectBeforeUpdate;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(catalog = "library")
+@EqualsAndHashCode(of = "id")
+@Getter
+@Setter
+@DynamicUpdate
+@DynamicInsert
+@SelectBeforeUpdate
+public class Genre {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    private String name;
+
+    @Basic(fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "genre")
+    private List<Book> books;
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+}
